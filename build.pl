b74489d54370f6f333ac4949f6c31ccbb031e724
John Daniel <jdaniel@etresoft.com>
2017-04-15 18:19:43 -0400

    Making more progress now. unixODBC is building.

--- build.pl
+++ build.pl
@@ -2,29 +2,30 @@
 
 use strict;
 
+use Cwd;
 use Getopt::Long qw(:config bundling pass_through);
 use File::Spec;
 use XML::LibXML;
+use File::Basename;
 
 # Get optional arguments.
 my $help;
-my $prefix;
-my $deploy;
+my $platform;
+my $architecture;
+my $deployment;
 my $debug;
 
 GetOptions
   (
   'help' => \$help,
-  'prefix=s' => \$prefix,
-  'deploy=s' => \$deploy,
+  'platform=s' => \$platform,
+  'architecture=s' => \$architecture,
+  'deployment=s' => \$deployment,
   'debug' => \$debug
   );
   
 die usage()
   if $help;
-
-$prefix = File::Spec->join($ENV{HOME}, 'Library')
-  if not $prefix;
 
 # Read packages to build, if any, from the command line.
 my @packageNamesToBuild = @ARGV;
@@ -41,19 +42,16 @@ foreach my $package ($dom->findnodes('/packages/package'))
     (
     name => $package->findvalue('name'),
     license => $package->findvalue('license/@type'),
-    bundleid => $package->findvalue('bundleid'),
-    version => $package->findvalue('version'),
     source => $package->findvalue('source'),
-    options => [],
-    env => {},
-    dependencies => []
+    prefix => $package->findvalue('prefix'),
+    platform => $package->findvalue('platform'),
+    architecture => $package->findvalue('architecture'),
+    cmake => $package->findvalue('cmake'),
+    configure => $package->findvalue('configure'),
+    build => $package->findvalue('build'),
+    install => $package->findvalue('install'),
+    env => {}
     );
-    
-  # Read the options.
-  foreach my $option ($package->findnodes('options/option/text()'))
-    {
-    push @{$package{options}}, $option;
-    }
     
   # Read the environment variables.
   foreach my $variable ($package->findnodes('env/*'))
@@ -64,15 +62,6 @@ foreach my $package ($dom->findnodes('/packages/package'))
     push @{$package{env}->{$name}}, $value;
     }
 
-  # Read the dependencies.
-  my @dependencies = $package->findnodes('dependencies/dependency/text()');
-  
-  foreach my $dependency (@dependencies)
-    {
-    push @{$package{dependencies}}, $dependency;
-    }
-
-  # Save the package.
   push @packages, \%package;
   
   # Save the package name.
@@ -85,22 +74,6 @@ foreach my $package ($dom->findnodes('/packages/package'))
 
 my %packageNamesToBuild = map { $_ => 1 } @packageNamesToBuild;
 
-# Add dependencies.
-foreach my $package (@packages)
-  {
-  next
-    if not $packageNamesToBuild{$package->{name}};
-    
-  foreach my $dependency (@{$package->{dependencies}})
-    {
-    my $dependencyFramework = 
-      File::Spec->join($prefix, "Frameworks", "$dependency.framework");
-    
-    $packageNamesToBuild{$dependency} = 1
-      if not -d $dependencyFramework;
-    }
-  }
-
 # Build the packages.
 foreach my $package (@packages)
   {
@@ -110,93 +83,269 @@ foreach my $package (@packages)
   build($package);
   }
 
-# Use lib2framework to build a package and stuff it into a framework.
+# Build a package.
 sub build
   {
   my $package = shift;
   
+	print "*** Building $package->{name} ***\n";
+
   my $name = $package->{name};
-  my $bundleID = $package->{bundleid};
-  my $version = $package->{version};
   my $source = $package->{source};
   my %env = %{$package->{env}};
-  my @args = @{$package->{options}};
 
-  # I know my dependencies will exist at this point, but I need to build a 
-  # PKG_CONFIG_PATH variable with all of them included.
-  # I should go ahead and add any executables to PATH too.
-  foreach my $dependency (@{$package->{dependencies}})
-    {
-    # Do the path first since I need to be extra careful with pkg-config.
-    my $dependencyFramework = 
-      File::Spec->join($prefix, "Frameworks", "$dependency.framework");
-    
-    my $dependencyPath = File::Spec->join($dependencyFramework, "Programs");
-        
-    $ENV{PATH} = "$ENV{PATH}:$dependencyPath";
-    
-    my $pkgConfigPath = 
-      File::Spec->join(
-        $dependencyFramework, "Versions/Current/unix/lib/pkgconfig");
-    
-    next
-      if not -d $pkgConfigPath;
+  # Set a default SDK.
+  my $sdk = 
+    $package->{platform}
+      ? $package->{platform}
+      : $platform;
       
-    $ENV{PKG_CONFIG_PATH} = "$ENV{PKG_CONFIG_PATH}:$pkgConfigPath";
-    }
-    
-	$ENV{PKG_CONFIG} = 
-		File::Spec->join(
-			$prefix, 
-			"Frameworks", 
-			"pkg-config.framework/Versions/Current/unix/bin/pkg-config");
+  $sdk = 'macosx'
+    if not $sdk;
 
-  push @args, "--deploy=$deploy"
-    if $deploy;
+  # Set a default architecture.
+  my $arch = 
+    $package->{architecture}
+      ? $package->{architecture}
+      : $architecture;
+      
+  $arch = 'x86_64'
+    if not $arch;
+
+  my $root = getcwd();
+  
+  my $prefix = 
+    $package->{prefix}
+      ? File::Spec->join($root, $package->{prefix})
+      : File::Spec->join($root, 'platforms', $sdk, $arch);
+      
+  # Setup the executable search path.
+  my $path = File::Spec->join($root, 'usr/bin');
+  
+  $ENV{PATH} = "$ENV{PATH}:$path";
     
-  push @args, "--debug"
-    if $debug;
-    
-  push @args, "--prefix=$prefix";
+  my $platformPath = File::Spec->join($root, 'platforms', $sdk, $arch, 'bin');
+  
+  $ENV{PATH} = "$ENV{PATH}:$platformPath";
+
+  # Setup the dynamic library search path.
+  my $platformLib = File::Spec->join($root, 'platforms', $sdk, $arch, 'lib');
+  
+  $ENV{DYLD_LIBRARY_PATH} = "$ENV{DYLD_LIBRARY_PATH}:$platformLib";
+
+  # Setup pkg-config.
+  my $pkgConfigPath = File::Spec->join($prefix, 'lib/pkgconfig');
+  
+  $ENV{PKG_CONFIG_PATH} = "$ENV{PKG_CONFIG_PATH}:$pkgConfigPath";
+
+  # Add extra parameters.    
+	my @configureargs;
+	my @cmakeargs;
+	
+  # Make sure prefix is set.
+  push @configureargs, "--prefix=$prefix";
+  push @cmakeargs, "-DCMAKE_INSTALL_PREFIX=$prefix";
   
   $ENV{PREFIX} = $prefix;
   
+  # Set environment variables.
   while(my ($variable, $values) = each %env)
     {
     $ENV{$variable} = "@{$values}";
     }
     
-  print(qq{PKG_CONFIG_PATH=$ENV{PKG_CONFIG_PATH}\n})
-    if $debug;
-  print(qq{PKG_CONFIG=$ENV{PKG_CONFIG}\n})
-    if $debug;
-  print(qq{PATH=$ENV{PATH}\n})
-    if $debug;
+	my $dir;
+
+	if(!-e $source)
+		{
+		printf("Source archive/directory '$source' not found\n");
+		die usage();
+		}
+	
+	my $archive = basename($source);
+
+	my $tmpdir = File::Spec->join('/tmp', $archive);
+
+	# If the archive is a directory, copy it to /tmp to work on it.
+	if(-d $source)
+		{
+		$dir = $tmpdir;
+	
+		runsystem(qq{/bin/rm -Rf "$dir"});
+		runsystem(qq{/bin/cp -R "$source" "$dir"});
+		}
+	else
+		{
+		runsystem(qq{/bin/rm -Rf "$tmpdir"});
+		runsystem(qq{/bin/mkdir -p "$tmpdir"});
+
+		if($archive =~ /\.tgz|\.tar\.gz$/)
+			{
+			runsystem(qq{/usr/bin/tar zxf "$source" "-C$tmpdir"});
+			} 
+		elsif($archive =~ /\.tbz|\.tar\.bz2$/)
+			{
+			runsystem(qq{/usr/bin/tar jxf "$source" "-C/tmp/$archive"});
+			}
+		elsif($archive =~ /\.zip$/)
+			{
+			runsystem(qq{/usr/bin/unzip "$source" -d "/tmp/$archive"});
+			}
+
+		$dir = `/usr/bin/find $tmpdir -mindepth 1 -maxdepth 1`;
+	
+		chomp $dir;
+		}
+
+  chdir($dir);
   
+	# Set a default deployment.  
+	if(not $deployment)
+		{
+		$deployment = `xcrun --sdk $sdk --show-sdk-version`;
+
+		chomp $deployment;
+		}
+  
+	# Setup the SDKROOT. Hopefully this will handle most situations.
+	my $sdkroot = `/usr/bin/xcrun --sdk $sdk --show-sdk-path`;
+  
+	chomp $sdkroot;
+
+	$ENV{SDKROOT} = $sdkroot;
+	
+	my $minversion = '';
+
+	# Configure for iPhone.
+	if($sdk =~ /^iphoneos/i)
+		{
+		$minversion = sprintf('-miphoneos-version-min=%s', $deployment);
+		
+		$ENV{IPHONEOS_DEPLOYMENT_TARGET} = $deployment;
+	
+		push @configureargs, qw(--build=x86_64-apple-darwin12 --host=arm-apple-darwin10);
+		
+		my $toolchain = File::Spec($root, 'iOS.cmake');
+		push @cmakeargs, "-DCMAKE_TOOLCHAIN_FILE=$toolchain";
+		push @cmakeargs, '-DIOS_PLATFORM=OS';
+		}
+  
+	# Configure for iPhone simulator.
+	elsif($sdk =~ /^iphonesimulator/i)
+		{
+		$minversion = sprintf('-mios-simulator-version-min=%s', $deployment);
+
+		$ENV{IPHONEOS_DEPLOYMENT_TARGET} = $deployment;
+		
+		my $toolchain = File::Spec($root, 'iOS.cmake');
+		push @cmakeargs, "-DCMAKE_TOOLCHAIN_FILE=$toolchain";
+		push @cmakeargs, '-DIOS_PLATFORM=SIMULATOR';
+		}
+	
+	# Configure for MacOS X.
+	elsif($sdk =~ /^macosx/i)
+		{
+		$minversion = sprintf('-mmacosx-version-min=%s', $deployment);
+
+		$ENV{MACOSX_DEPLOYMENT_TARGET} = $deployment;
+		}
+
+	# Setup the isysroot command-line parameters and PATH, if necessary.
+	$ENV{PATH} = sprintf(
+		'%s:%s:/usr/bin:/bin:/usr/sbin:/sbin',
+		$sdkroot,
+		'/Applications/Xcode.app/Contents/Developer/usr/bin')
+	if $sdk && ($sdk =~ /^iphone/i);
+
+  my $cflags = '';
+  my $cxxflags = '';
+  
+  $cflags .= "-arch $arch "
+    if $architecture;
+
+  $cxxflags .= "-arch $arch "
+    if $architecture;
+
+  $cflags .= "-isysroot $sdkroot "
+    if $sdkroot;
+
+  $cxxflags .= "-isysroot $sdkroot "
+    if $sdkroot;
+
+  $cflags .= "$minversion "
+    if $minversion;
+
+  $cxxflags .= "$minversion "
+    if $minversion;
+
+	$ENV{CFLAGS} = $cflags;
+	$ENV{CXXFLAGS} = $cxxflags;
+
+  # Debug statements.
   if($debug)
     {
+		print(qq{PKG_CONFIG_PATH=$ENV{PKG_CONFIG_PATH}\n});
+		print(qq{PATH=$ENV{PATH}\n});
+		print(qq{DYLD_LIBRARY_PATH=$ENV{DYLD_LIBRARY_PATH}\n});
+		print(qq{PREFIX=$ENV{PREFIX}\n});
+		print(qq{SDKROOT=$ENV{PREFIX}\n});
+		print(qq{IPHONEOS_DEPLOYMENT_TARGET=$ENV{IPHONEOS_DEPLOYMENT_TARGET}\n});
+		print(qq{MACOSX_DEPLOYMENT_TARGET=$ENV{MACOSX_DEPLOYMENT_TARGET}\n});
+		print(qq{CFLAGS=$ENV{CFLAGS}\n});
+		print(qq{CXXFLAGS=$ENV{CXXFLAGS}\n});
+		
     foreach my $env (keys %env)
       {
       print(qq{$env=$ENV{$env}\n})
       }
     }
     
-  print(qq{lib2framework $name $bundleID $version $source @args\n})
+	runsystem(qq{$package->{cmake} @cmakeargs})
+		if $package->{cmake};
+
+	runsystem(qq{$package->{configure} @configureargs})
+		if $package->{configure};
+
+	runsystem(qq{$package->{build}})
+		if $package->{build};
+
+  runsystem(qq{$package->{install}})
+	  if $package->{install};
+	  
+	chdir($root);
+	
+	runsystem(qq{rm -Rf $tmpdir})
+	  if not $debug;
+	  
+	print "*** Done building $package->{name} ***\n";
+  }
+
+sub debug
+  {
+  my @args = @_;
+  
+  print("DEBUG: @args\n")
     if $debug;
+  }
+  
+sub runsystem
+  {
+  debug(@_);
     
-  system(qq{lib2framework $name $bundleID $version $source @args});
+  system(@_);
   }
 
 # Print usage messages.
 sub usage
   {
   my $message = << "EOF";
-Usage: build.sh [options] [package(s)]
+Usage: build.pl [options] [package(s)]
 
   where [options] can be:
     --help This help text
-    --prefix=<installation path>
-    --deploy=<target macOS version>
+    --platform=macosx|iphoneos|iphonesimulator
+    --architecture=i386|x86_64|arm7|arm64
+    --deployment=<deployment version>
     --debug For debugging statements
     
   and [package(s)] is an optional list of packages to build. 
